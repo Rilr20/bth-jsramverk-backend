c9d68753101d8862e7063b815204eba36b23ed8a
Rilr20 <nrrl99@gmail.com>
2022-09-22 09:30:55 +0100

    removed test parameter

--- db/database.js
+++ db/database.js
@@ -2,13 +2,13 @@ const mongo = require("mongodb").MongoClient;
 // const collectionName = "docs";
 
 const database = {
-    getDb: async function getDb(collectionName, test=true) {
+    getDb: async function getDb(collectionName) {
         let clstr = "cluster0.8ifmldd.mongodb.net/?retryWrites=true&w=majority";
         let mngdb = "mongodb+srv://";
         let dsn = `${mngdb}${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@${clstr}`;
 
         // console.log(dsn);
-        if (test) {
+        if (process.env.NODE_ENV === 'test') {
             dsn = "mongodb://127.0.0.1:27017";
         }
 
