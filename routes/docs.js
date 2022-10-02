15c0031d36dda7ca26d7d5a2b44a5e464eb491e0
Rilr20 <nrrl99@gmail.com>
2022-10-02 12:37:52 +0200

    post request also changes user collection

--- routes/docs.js
+++ routes/docs.js
@@ -17,21 +17,48 @@ router.get(
     }
 );
 
-// router.get("/", async (req, res) => {
+async function addDocToUser(docId, permission, email) {
+    let filter = { email: email };
+    const db = await database.getDb("users");
+    let resultSet = await db.collection.find(filter, {}).toArray();
+    let docs = resultSet[0].docs;
 
-
-// });
+    console.log(resultSet);
+    if (resultSet !== 0) {
+        console.log("här är jag");
+        if (docs === undefined) {
+            docs = [];
+            docs[0] = {
+                docId: docId,
+                write: permission
+            };
+        } else {
+            docs.push({ docId: docId, write: permission });
+        }
+        resultSet = await db.collection.updateOne(filter, { $set: { docs: docs } });
+    }
+}
 
 router.post("/", async (req, res) => {
     //CREATE document to database
     const title = req.body.title;
     const text = req.body.text;
-    const email = req.body.email;
+    const email = req.body.email.toLowerCase();
     const code = req.body.code;
-
+    const write = req.body.write;
+    //Ändra i user fältet också, där ska vara doc id och permission!
+    //då måste man spara id från resultset
+    //sedan använda det i nästa
+    //göra det till en function???
     const db = await database.getDb("docs");
-    const resultSet = await db.collection.insertOne({ title: title, text: text, email: email, code: code });
+    const resultSet = await db.collection.insertOne({
+        title: title,
+        text: text,
+        email: email,
+        code: code,
+    });
 
+    await addDocToUser(resultSet.insertedId, write, email);
     await db.client.close();
     if (resultSet.acknowledged) {
         res.status(201).json({
