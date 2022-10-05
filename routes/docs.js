237d4d2eab48b2eacb13edc81c6c6fa97cb1463d
Rilr20 <nrrl99@gmail.com>
2022-10-05 18:24:50 +0200

    fixed problem if invalid email was sent

--- routes/docs.js
+++ routes/docs.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const database = require("../db/database");
 const auth = require('../modules/auth');
 
-
-
 router.get(
     "/",
     (req, res, next) => auth.checkToken(req, res, next),
@@ -20,47 +18,57 @@ router.get(
 async function addDocToUser(docId, permission, email) {
     let filter = { email: email };
     const db = await database.getDb("users");
-    let resultSet = await db.collection.find(filter, {}).toArray();
-    let docs = resultSet[0].docs;
+    let resultSet = await db.collection.findOne(filter, {});
 
-    console.log(resultSet);
-    if (resultSet !== 0) {
-        console.log("här är jag");
-        if (docs === undefined) {
-            docs = [];
-            docs[0] = {
-                docId: docId,
-                write: permission
-            };
-        } else {
-            docs.push({ docId: docId, write: permission });
+    if (resultSet === null) {
+        return "err";
+    } else {
+        let docs = resultSet.docs;
+
+        // console.log(resultSet);
+        if (resultSet !== 0) {
+            console.log("här är jag");
+            if (docs === undefined) {
+                docs = [];
+                docs[0] = {
+                    docId: docId,
+                    write: permission
+                };
+            } else {
+                docs.push({ docId: docId, write: permission });
+            }
+            await db.collection.updateOne(filter, { $set: { docs: docs } });
         }
-        resultSet = await db.collection.updateOne(filter, { $set: { docs: docs } });
     }
 }
 
 router.post("/", async (req, res) => {
     //CREATE document to database
+    // console.log(req.body);
     const title = req.body.title;
     const text = req.body.text;
     const email = req.body.email.toLowerCase();
     const code = req.body.code;
     const write = req.body.write;
-    //Ändra i user fältet också, där ska vara doc id och permission!
-    //då måste man spara id från resultset
-    //sedan använda det i nästa
-    //göra det till en function???
     const db = await database.getDb("docs");
     const resultSet = await db.collection.insertOne({
         title: title,
         text: text,
         email: email,
         code: code,
+        access: [{ user: email, write: write }]
     });
 
-    await addDocToUser(resultSet.insertedId, write, email);
+    let err = await addDocToUser(resultSet.insertedId, write, email);
+
     await db.client.close();
-    if (resultSet.acknowledged) {
+    if (err === "err") {
+        res.status(400).json({
+            data: {
+                msg: "400: user doesn't exist"
+            }
+        });
+    } else if (resultSet.acknowledged) {
         res.status(201).json({
             data: {
                 msg: "201; Added an object!",
