20fd656f5c2951b9c0be7c9797c1afb28dba672e
Rilr20 <nrrl99@gmail.com>
2022-10-14 20:51:32 +0200

    fixed test token

--- modules/auth.js
+++ modules/auth.js
@@ -1,14 +1,13 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+/*eslint max-len: ["error", { "code": 141 }]*/
+const secret = process.env.NODE_ENV === 'test' ? "8927345684584568456868468456845680670645685980257890125789126124" : process.env.JWT_SECRET;
+
 const auth = {
     checkToken: function (req, res, next) {
         const token = req.headers['x-access-token'];
-        let secret = process.env.JWT_SECRET;
 
-        if (process.env.NODE_ENV === 'test') {
-            secret = "8927345684584568456868468456845680670645685980257890125789126124";
-        }
 
         // console.log(process.env.JWT_SECRET);
         jwt.verify(token, secret, function (err) {
